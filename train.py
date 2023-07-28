b228e66bf9e69eba9e7942842371bc383b28308e
Ritesh Soun <sounritesh@gmail.com>
2023-07-28 19:48:40 +0530

    init

--- train.py
+++ train.py
@@ -20,9 +20,9 @@ model_params={
     "MODEL":"t5-base",             # model_type: t5-base/t5-large
     "TRAIN_BATCH_SIZE":8,          # training batch size
     "VALID_BATCH_SIZE":8,          # validation batch size
-    "TRAIN_EPOCHS":3,              # number of training epochs
+    "TRAIN_EPOCHS":20,              # number of training epochs
     "VAL_EPOCHS":1,                # number of validation epochs
-    "LEARNING_RATE":1e-4,          # learning rate
+    "LEARNING_RATE":1e-5,          # learning rate
     "MAX_SOURCE_TEXT_LENGTH":64,  # max length of source text
     "MAX_TARGET_TEXT_LENGTH":32,   # max length of target text
     "SEED": 42                     # set seed for reproducibility
