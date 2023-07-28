36591108c7e2eb9cb48bfd60e03d46b584d5cfe6
Ritesh Soun <sounritesh@gmail.com>
2023-07-28 19:30:51 +0530

    init

---
+++ data/dataset.py
@@ -0,0 +1,52 @@
+from torch.data.utils import Dataset
+import torch
+
+class ProductLabelsDataset(Dataset):
+    def __init__(self, dataframe, tokenizer,source_len = 64, target_len = 32):
+        self.tokenizer = tokenizer
+        self.dataframe = dataframe
+        self.source_len = source_len
+        self.target_len = target_len
+
+        self.source_text = []
+        for _, row in self.dataframe.iterrows():
+            self.source_text.append(" | ".join([row["a1"], row["a2"], row["a3"], row["a4"]]))
+        self.target_text = self.dataframe["caption"].values
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        source_text = str(self.source_text[index])
+        target_text = str(self.target_text[index])
+
+        source = self.tokenizer.batch_encode_plus(
+            [source_text],
+            max_length = self.source_len,
+            pad_to_max_length = True,
+            truncation = True,
+            padding = "max_length",
+            return_tensors = "pt"
+        )
+
+        target = self.tokenizer.batch_encode_plus(
+            [target_text],
+            max_length = self.target_len,
+            pad_to_max_length = True,
+            truncation = True,
+            padding = "max_length",
+            return_tensors = "pt"
+        )
+
+        source_ids = source["input_ids"].squeeze()
+        source_mask = source["attention_mask"].squeeze()
+        target_ids = target["input_ids"].squeeze()
+        target_mask = target["attention_mask"].squeeze()
+
+        return {
+            "source_ids": source_ids.to(dtype=torch.long),
+            "source_mask": source_mask.to(dtype=torch.long),
+            "target_ids": target_ids.to(dtype=torch.long),
+            "target_ids_y": target_ids.to(dtype=torch.long),
+        }
+
---
+++ src/train.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from argparse import ArgumentParser
+from torch import cuda
+from .trainer import T5Trainer
+
+parser = ArgumentParser()
+
+parser.add_argument("--data-file", type=str, required=True)
+parser.add_argument("--output-dir", type=str, default="./output/")
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.data_file)
+print(df.sample(10))
+
+device = 'cuda' if cuda.is_available() else 'cpu'
+
+
+model_params={
+    "MODEL":"t5-base",             # model_type: t5-base/t5-large
+    "TRAIN_BATCH_SIZE":8,          # training batch size
+    "VALID_BATCH_SIZE":8,          # validation batch size
+    "TRAIN_EPOCHS":3,              # number of training epochs
+    "VAL_EPOCHS":1,                # number of validation epochs
+    "LEARNING_RATE":1e-4,          # learning rate
+    "MAX_SOURCE_TEXT_LENGTH":512,  # max length of source text
+    "MAX_TARGET_TEXT_LENGTH":50,   # max length of target text
+    "SEED": 42                     # set seed for reproducibility
+
+}
+
+T5Trainer(dataframe=df, model_params=model_params, device=device, output_dir=args.output_dir)
